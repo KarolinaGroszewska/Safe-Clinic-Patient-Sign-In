dd922a6e5984665bc1bcc521aeca4067a562cbee
kgroszewska0121 <kgroszewska0121@stu.d214.org>
2020-07-11 01:29:17 -0500

    scrollable so-called PDF readers

--- Safe Clinic Patient Sign In/AgencyAdultFormSpanishViewController.swift
+++ Safe Clinic Patient Sign In/AgencyAdultFormSpanishViewController.swift
@@ -9,11 +9,33 @@
 import UIKit
 
 class AgencyAdultFormSpanishViewController: UIViewController {
-
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
 
-        // Do any additional setup after loading the view.
+        scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 23.0
+        scrollView.addSubview(stackView)
+
+        scrollView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[stackView]|", options: NSLayoutConstraint.FormatOptions.alignAllCenterX, metrics: nil, views: ["stackView": stackView as Any]))
+        scrollView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[stackView]|", options: NSLayoutConstraint.FormatOptions.alignAllCenterX, metrics: nil, views: ["stackView": stackView as Any]))
+        let imageName = "Screen Shot 2020-07-07 at 12.55.54 PM"
+        let image = UIImage(named: imageName)
+        let imageView = UIImageView(image: image!)
+        imageView.frame = CGRect(x: 0, y: 0, width: 768, height: 1024)
+        stackView.addArrangedSubview(imageView)
+        //Imageview on Top of View
+        self.view.bringSubviewToFront(imageView)        // Do any additional setup after loading the view.
     }
     
 
