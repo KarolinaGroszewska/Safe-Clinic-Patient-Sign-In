afd9218c3d412479317036b5192f2c8bfe5bdb9a
kgroszewska0121 <kgroszewska0121@stu.d214.org>
2020-07-10 19:02:14 -0500

    Initial Commit

---
+++ Safe Clinic Patient Sign In/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Safe Clinic Patient Sign In
+//
+//  Created by Karolina on 7/10/20.
+//  Copyright © 2020 Karolina Groszewska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
