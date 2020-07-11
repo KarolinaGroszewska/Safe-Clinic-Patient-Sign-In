790d9434a36f0f81ed5ee7395508e51813aafd28
kgroszewska0121 <kgroszewska0121@stu.d214.org>
2020-07-10 22:01:20 -0500

    Adding in VCs and Storyboard Views

---
+++ Safe Clinic Patient Sign In/AgencyCompletionEnglishController.swift
@@ -0,0 +1,30 @@
+//
+//  AgencyCompletionEnglishController.swift
+//  Safe Clinic Patient Sign In
+//
+//  Created by Karolina on 7/10/20.
+//  Copyright © 2020 Karolina Groszewska. All rights reserved.
+//
+
+import UIKit
+
+class AgencyCompletionEnglishController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
